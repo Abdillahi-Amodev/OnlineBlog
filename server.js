5c56d9c78dd37e302de5193b87eb0d874f16a362
Amodev <bs141.19109906.1@gmail.com>
2021-07-18 16:56:18 +0200

    saved change by me

---
+++ server.js
@@ -0,0 +1,23 @@
+const express= require('express')
+const app =express();
+require('dotenv').config()
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send('Amodev online cool')
+
+})
+
+const port =process.env.PORT || 8080;
+
+
+
+
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(`listening port ${port}` );
+})
