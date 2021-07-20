f0c760972b8b3884de81b43e6b169f408a3ce5fd
Amodev <bs141.19109906.1@gmail.com>
2021-07-20 16:49:55 +0200

    testing

--- controller/route.js
+++ controller/route.js
@@ -12,7 +12,7 @@ route.get('/admin', controller.getAdmin)
 route.get('/admin/dashboard', controller.getAdminDashboard)
 route.get('/admin/post', controller.getAdminPost)
 route.post('/admin/post', controller.postAdmin)
-// route.get('/:id', controller.getSingleBlogs)
+route.get('/:id', controller.getSingleBlogs)
 
 
 
